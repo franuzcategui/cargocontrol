4351a07d055de369a0d19aee6c2c357b30b17c3c
usmanasad87 <usmanasad0324@gmail.com>
2024-01-07 18:00:04 +0500

    Viejes Details updated

--- lib/common_widgets/carga_widget.dart
+++ lib/common_widgets/carga_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cargocontrol/core/extensions/color_extension.dart';
+import 'package:cargocontrol/features/admin/create_vessel/controllers/ad_vessel_controller.dart';
 import 'package:cargocontrol/features/admin/create_vessel/widgets/preliminatr_tile.dart';
 import 'package:cargocontrol/models/viajes_models/viajes_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -64,81 +65,119 @@ class CargaWidget extends StatelessWidget {
             );
           },
         ),
+        Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            return ref
+                .watch(fetchCurrentVesselsProvider)
+                .when(
+              data: (vesselModel) {
+                return Column(
+                  children: [
+                    CustomTile(
+                      title: "Procedencia",
+                      subText: vesselModel.entryPort
+                    ),
+                  ],
+                );
+              },
+              error: (error, st) {
+                debugPrintStack(stackTrace: st);
+                debugPrint(error.toString());
+                return const SizedBox();
+              },
+              loading: () {
+                return const SizedBox();
+              },
+            );
+          },
+        ),
+        if( viajesModel.entryTimeTruckWeightToPort!=0.0)
+          CustomTile(
+              title: "Peso tara ",
+              subText: viajesModel.entryTimeTruckWeightToPort.toString()
+          ),
         if( viajesModel.exitTimeTruckWeightToPort!=0.0)
         CustomTile(
             title: "Peso bruto de salida",
             subText: viajesModel.exitTimeTruckWeightToPort.toString()
         ),
+        if( viajesModel.exitTimeTruckWeightToPort!=0.0)
+          CustomTile(
+              title: "Total de carga inicial",
+              subText: (viajesModel.exitTimeTruckWeightToPort - viajesModel.entryTimeTruckWeightToPort).toString()
+          ),
         if( viajesModel.cargoUnloadWeight!=0.0)
 
           CustomTile(
             title: "Peso bruto de llegada",
             subText: viajesModel.cargoUnloadWeight.toString()
         ),
-        if( viajesModel.entryTimeTruckWeightToPort!=0.0)
 
-          CustomTile(
-            title: "Peso tara ",
-            subText: viajesModel.entryTimeTruckWeightToPort.toString()
-        ),
         if( viajesModel.cargoUnloadWeight!=0.0)
           CustomTile(
-            title: "Peso neto de llegada",
+            title: "Total de carga final",
             subText:(viajesModel.cargoUnloadWeight - viajesModel.entryTimeTruckWeightToPort).toString()
         ),
-        if( viajesModel.cargoUnloadWeight!=0.0)
-       CustomTile(
-            title: "Deficit de chofer",
-            subText: (viajesModel.exitTimeTruckWeightToPort -viajesModel.cargoUnloadWeight ).toString(),
-          isGoodSign: true,
-        ),
-        Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) {
-            return ref
-                .watch(getIndustriaIndustryByIndustryId(viajesModel.industryId))
-                .when(
-              data: (industryModel) {
-                return CustomTile(
-                  title: "Deficit promedio industria",
-                  subText: industryModel.deficit.toString(),
-                  isGoodSign: true,
-                );
-              },
-              error: (error, st) {
-                debugPrintStack(stackTrace: st);
-                debugPrint(error.toString());
-                return const SizedBox();
-              },
-              loading: () {
-                return const SizedBox();
-              },
-            );
-          },
-        ),
+        if( viajesModel.cargoUnloadWeight!=0.0)...[
+          CustomTile(
+            title: "Pérdida de viaje (kg)",
+            subText: (viajesModel.cargoUnloadWeight - viajesModel.exitTimeTruckWeightToPort).toString(),
+            isGoodSign: true,
+          ),
+          CustomTile(
+            title: "Pérdida de viaje (%)",
+            subText: "${(((viajesModel.cargoUnloadWeight - viajesModel.entryTimeTruckWeightToPort) - (viajesModel.exitTimeTruckWeightToPort - viajesModel.entryTimeTruckWeightToPort))/(viajesModel.exitTimeTruckWeightToPort - viajesModel.entryTimeTruckWeightToPort)).toStringAsFixed(2)}%",
 
-        Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) {
-            return ref
-                .watch(getChoferModelByNationalId(viajesModel.chofereId))
-                .when(
-              data: (model) {
-                return CustomTile(
-                  title: "Deficit promedio de chofer",
-                  subText: model.averageCargoDeficit.toString(),
-                  isGoodSign: true,
-                );
-              },
-              error: (error, st) {
-                debugPrintStack(stackTrace: st);
-                debugPrint(error.toString());
-                return const SizedBox();
-              },
-              loading: () {
-                return const SizedBox();
-              },
-            );
-          },
-        ),
+          ),
+          Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              return ref
+                  .watch(getIndustriaIndustryByIndustryId(viajesModel.industryId))
+                  .when(
+                data: (industryModel) {
+                  return CustomTile(
+                    title: "Deficit promedio industria",
+                    subText: "${"-"+(industryModel.deficit/(industryModel.cargoUnloaded + industryModel.deficit)).toStringAsFixed(2)}%",
+                    isGoodSign: true,
+                  );
+                },
+                error: (error, st) {
+                  debugPrintStack(stackTrace: st);
+                  debugPrint(error.toString());
+                  return const SizedBox();
+                },
+                loading: () {
+                  return const SizedBox();
+                },
+              );
+            },
+          ),
+          //Todo usman : this is remaining
+          Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              return ref
+                  .watch(getChoferModelByNationalId(viajesModel.chofereId))
+                  .when(
+                data: (model) {
+                  return CustomTile(
+                    title: "Deficit promedio de chofer",
+                    subText: model.averageCargoDeficit.toString(),
+                    isGoodSign: true,
+                  );
+                },
+                error: (error, st) {
+                  debugPrintStack(stackTrace: st);
+                  debugPrint(error.toString());
+                  return const SizedBox();
+                },
+                loading: () {
+                  return const SizedBox();
+                },
+              );
+            },
+          ),
+        ]
+
 
 
 
